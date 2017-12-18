e1d92410ee49b6623dac2571a686fe02cfa5d715
WAND\Alecsandru <alecu89@yahoo.com>
2017-12-18 14:09:41 +0200

    forgot file

---
+++ Tema1B_FMSE/SyntaxNodes/DomainValueSyntaxNode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1B_FMSE.SyntaxNodes
+{
+    public class DomainValueSyntaxNode : SyntaxNode
+    {
+        public LiteralSyntaxNode Operator { get; set; }
+
+        public EOperationKinds OperationKind { get; set; }
+
+        public ValueSyntaxNode InnerValue { get; set; }
+
+
+        public override void AssignChild(SyntaxNode child)
+        {
+            if (Operator == null)
+            {
+                var literalSyntaxNode = (LiteralSyntaxNode)child;
+
+                Operator = literalSyntaxNode;
+                literalSyntaxNode.Parent = this;
+                StartIndex = literalSyntaxNode.StartIndex;
+                EndIndex = literalSyntaxNode.EndIndex;
+
+                OperationKind = OperatorsInformation.MappedOperationKinds[literalSyntaxNode.LiteralValue];
+
+                return;
+            }
+
+            if (InnerValue == null)
+            {
+                ValueSyntaxNode valueSyntaxNode = (ValueSyntaxNode)child;
+
+                InnerValue = valueSyntaxNode;
+                valueSyntaxNode.Parent = this;
+                EndIndex = valueSyntaxNode.EndIndex;
+                _isFinishedReading = true;
+            }
+        }
+
+        public override IEnumerable<SyntaxNode> Children
+        {
+            get
+            {
+                return new List<SyntaxNode> { Operator, InnerValue };
+            }
+        }
+    }
+}
