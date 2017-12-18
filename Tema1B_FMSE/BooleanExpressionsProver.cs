284a1cd4de00b7f1e7658074499262e38b1b5468
DESKTOP-8C812IN\alecs <alecu89@yahoo.com>
2017-12-18 03:52:52 +0200

    finally added option to check if satisfiable

---
+++ Tema1B_FMSE/BooleanExpressionsProver.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tema1B_FMSE.SyntaxNodes;
+using Microsoft.Z3;
+
+namespace Tema1B_FMSE
+{
+    public class BooleanExpressionsProver
+    {
+        private Dictionary<string, BoolExpr> _createdSymbols;
+        private Context _currentContext;
+        private BoolExpr _expressionTree;
+
+        public bool IsSatisfiable(SyntaxTree syntaxTree)
+        {
+            Initialize();
+            ConstructProof(syntaxTree);
+
+            return CheckIfIsSatisfiable();
+        }
+
+        private bool CheckIfIsSatisfiable()
+        {
+            var solver = _currentContext.MkSolver();
+            solver.Assert(_expressionTree);
+            var status = solver.Check();
+
+            if (status == Status.SATISFIABLE)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void ConstructProof(SyntaxTree syntaxTree)
+        {
+            _expressionTree = ConstructExpression(syntaxTree.RootValue);
+        }
+
+        private BoolExpr ConstructExpression(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is BlockExpressionSyntaxNode)
+            {
+                var blockExpressionSyntaxNode = (BlockExpressionSyntaxNode) syntaxNode;
+
+                return ConstructExpression(blockExpressionSyntaxNode.InnerValue);
+            }
+
+            if (syntaxNode is SymbolSyntaxNode)
+            {
+                var symbolSyntaxNode = (SymbolSyntaxNode)syntaxNode;
+
+                if (_createdSymbols.ContainsKey(symbolSyntaxNode.Id))
+                {
+                    return _createdSymbols[symbolSyntaxNode.Id];
+                }
+
+                var boolExpr = _currentContext.MkBoolConst(symbolSyntaxNode.Id);
+                _createdSymbols[symbolSyntaxNode.Id] = boolExpr;
+                return boolExpr;
+            }
+
+            if (syntaxNode is BinaryExpressionSyntaxNode)
+            {
+                var binaryExpressionSyntaxNode = (BinaryExpressionSyntaxNode) syntaxNode;
+
+                var leftExpression = ConstructExpression(binaryExpressionSyntaxNode.LeftValue);
+                var rightExpression = ConstructExpression(binaryExpressionSyntaxNode.RightValue);
+
+                if (binaryExpressionSyntaxNode.OperationKind == EOperationKinds.And)
+                {
+                    return _currentContext.MkAnd(leftExpression, rightExpression);
+                }
+
+                if (binaryExpressionSyntaxNode.OperationKind == EOperationKinds.Or)
+                {
+                    return _currentContext.MkOr(leftExpression, rightExpression);
+                }
+
+                if (binaryExpressionSyntaxNode.OperationKind == EOperationKinds.Implication)
+                {
+                    return _currentContext.MkImplies(leftExpression, rightExpression);
+                }
+            }
+
+            if (syntaxNode is UnaryExpressionSyntaxNode)
+            {
+                var unaryExpressionSyntaxNode = (UnaryExpressionSyntaxNode) syntaxNode;
+                var innerExpression = ConstructExpression(unaryExpressionSyntaxNode.InnerValue);
+
+                if (unaryExpressionSyntaxNode.OperationKind == EOperationKinds.Not)
+                {
+                    return _currentContext.MkNot(innerExpression);
+                }
+            }
+
+            return null;
+        }
+
+        private void Initialize()
+        {
+            _currentContext = new Context();
+            _createdSymbols = new Dictionary<string, BoolExpr>();
+        }
+    }
+}
--- Tema1B_FMSE/MainWindow.xaml.cs
+++ Tema1B_FMSE/MainWindow.xaml.cs
@@ -26,37 +26,7 @@ namespace Tema1B_FMSE
         {
             InitializeComponent();
         }
-        /*
-         *   Goal g = ctx.MkGoal(true);
-
-            ArraySort asort = ctx.MkArraySort(ctx.IntSort, ctx.MkBitVecSort(32));
-
-            ArrayExpr aex = (ArrayExpr)ctx.MkConst(ctx.MkSymbol("MyArray"), asort);
-
-            Expr sel = ctx.MkSelect(aex, ctx.MkInt(0));
-
-            g.Assert(ctx.MkEq(sel, ctx.MkBV(42, 32)));
-
-            Symbol xs = ctx.MkSymbol("x");
-
-            IntExpr xc = (IntExpr)ctx.MkConst(xs, ctx.IntSort);
-
-
-
-            Symbol fname = ctx.MkSymbol("f");
-
-            Sort[] domain = { ctx.IntSort };
-
-            FuncDecl fd = ctx.MkFuncDecl(fname, domain, ctx.IntSort);
-
-            Expr[] fargs = { ctx.MkConst(xs, ctx.IntSort) };
-
-            IntExpr fapp = (IntExpr)ctx.MkApp(fd, fargs);
-
-
-
-            g.Assert(ctx.MkEq(ctx.MkAdd(xc, fapp), ctx.MkInt(123)));
-        */
+ 
 
         private void BtnProve_OnClick(object sender, RoutedEventArgs e)
         {
@@ -65,6 +35,19 @@ namespace Tema1B_FMSE
             var parsedSyntaxTree = syntaxTreeParser.Parse(txtExpression.Text);
 
             trViewSyntaxTree.ItemsSource = new List<SyntaxNode> {parsedSyntaxTree};
+
+            var booleanExpressionsProver = new BooleanExpressionsProver();
+
+            var isSatifiable = booleanExpressionsProver.IsSatisfiable(parsedSyntaxTree);
+
+            if (isSatifiable)
+            {
+                lblIsSatisfiableVal.Content = "Yes";
+            }
+            else
+            {
+                lblIsSatisfiableVal.Content = "No";
+            }
         }
     }
 }
