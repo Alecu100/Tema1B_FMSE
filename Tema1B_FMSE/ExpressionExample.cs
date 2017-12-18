dee6deba7c18830eedb25fb9dfa1c8a3b8a65004
WAND\Alecsandru <alecu89@yahoo.com>
2017-12-18 15:19:45 +0200

    sadfasdf

---
+++ Tema1B_FMSE/ExpressionExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1B_FMSE
+{
+    public class ExpressionExample 
+    {
+        public string Text { get; set; }
+
+        public ExpressionExample(string text)
+        {
+            Text = text;
+        }
+    }
+}
